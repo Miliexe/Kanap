38f2d24f30c931e513dbc2b992a0a73882fa9388
Miliexe <emilie.lblc@gmail.com>
2022-08-12 15:32:59 +0200

    Update Product insertion + Init url by id

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,8 @@
 console.log("Page produit");
+console.log(window.location);
+
+let url = new URL(window.location)
+
+let id= url.searchParams.get("id")
+
+console.log(id);
--- front/js/script.js
+++ front/js/script.js
@@ -5,17 +5,35 @@ fetch("http://localhost:3000/api/products/")
     .then((product) => product.json())
     .then((products) => {
 
-        //products insertion into "items" blocs
-        for(let product in products) {
-            document.getElementById("items")
-            .innerHTML +=
-            `<a href="product.html?id=${products[product]._id}">
-                <article>
-                    <img src="${products[product].imageUrl}" alt="${products[product].altTxt}">
-                    <h3 class="ProductName">${products[product].name}</h3>
-                    <p class="productDescription">${products[product].description}</p>
-                </article>
-            </a>`
-            console.log("item create succesfull");
-        };
+    showProducts(products)
+        
     })
+
+    //products insertion into "items" blocs
+    function showProducts(products){
+        
+        for(let product in products) {
+            let sectionItems= document.getElementById("items");
+            let a = document.createElement("a");
+            a.href=`product.html?id=${products[product]._id}`
+            sectionItems.appendChild(a);
+
+            let article = document.createElement("article");
+            a.appendChild(article);
+
+            let img = document.createElement("img");
+            img.src= `${products[product].imageUrl}`;
+            img.alt= `${products[product].altTxt}`;
+            article.appendChild(img);
+
+            let h3 = document.createElement("h3");
+            h3.classList.add("ProductName");
+            h3.innerText = `${products[product].name}`
+            article.appendChild(h3);
+
+            let p = document.createElement("p");
+            p.classList.add("ProductDescription");
+            p.innerText = `${products[product].description}`
+            article.appendChild(p);
+        };    
+    }
