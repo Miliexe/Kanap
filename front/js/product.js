2e5b8e487129a97233846240d62290c7b0e30ee8
Miliexe <emilie.lblc@gmail.com>
2022-08-15 13:29:53 +0200

    Init showProducts

--- front/js/product.js
+++ front/js/product.js
@@ -6,3 +6,34 @@ let url = new URL(window.location)
 let id= url.searchParams.get("id")
 
 console.log(id);
+
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then((data) => data.json())
+    .then((product) => {
+
+    showProduct(product)
+        
+    })
+
+function showProduct(product){
+
+    let divImg = document.querySelector(".item__img");
+    let img = document.createElement("img");
+    img.src= `${product.imageUrl}`;
+    img.alt= `${product.altTxt}`;
+    divImg.appendChild(img);
+
+    let title = document.getElementById("title");
+    title.innerText = `${product.name}`;
+
+    let price = document.getElementById("price");
+    price.innerText = new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(product.price); 
+
+    let description = document.getElementById("description");
+    description.innerText = `${product.description}`;
+
+    for(let colors in product.colors){
+        let color = document.getElementById("colors");
+        color.innerHTML += `<option value="${product.colors[colors]}">${product.colors[colors]}</option>`;
+    }
+}
--- front/js/script.js
+++ front/js/script.js
@@ -12,28 +12,28 @@ fetch("http://localhost:3000/api/products/")
     //products insertion into "items" blocs
     function showProducts(products){
         
-        for(let product in products) {
+        for(let product of products) {
             let sectionItems= document.getElementById("items");
             let a = document.createElement("a");
-            a.href=`product.html?id=${products[product]._id}`
+            a.href=`product.html?id=${product._id}`
             sectionItems.appendChild(a);
 
             let article = document.createElement("article");
             a.appendChild(article);
 
             let img = document.createElement("img");
-            img.src= `${products[product].imageUrl}`;
-            img.alt= `${products[product].altTxt}`;
+            img.src= `${product.imageUrl}`;
+            img.alt= `${product.altTxt}`;
             article.appendChild(img);
 
             let h3 = document.createElement("h3");
             h3.classList.add("ProductName");
-            h3.innerText = `${products[product].name}`
+            h3.innerText = `${product.name}`
             article.appendChild(h3);
 
             let p = document.createElement("p");
             p.classList.add("ProductDescription");
-            p.innerText = `${products[product].description}`
+            p.innerText = `${product.description}`
             article.appendChild(p);
         };    
     }
