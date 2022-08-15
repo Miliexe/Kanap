a185c7f0d246578dc2e9cca4635def0209f03413
Miliexe <emilie.lblc@gmail.com>
2022-08-15 18:31:40 +0200

    Init addToCart

--- front/js/product.js
+++ front/js/product.js
@@ -3,37 +3,75 @@ console.log(window.location);
 
 let url = new URL(window.location)
 
-let id= url.searchParams.get("id")
+let id = url.searchParams.get("id")
 
 console.log(id);
 
 fetch(`http://localhost:3000/api/products/${id}`)
-    .then((data) => data.json())
-    .then((product) => {
+  .then((data) => data.json())
+  .then((product) => {
 
     showProduct(product)
-        
-    })
 
-function showProduct(product){
+  })
 
-    let divImg = document.querySelector(".item__img");
-    let img = document.createElement("img");
-    img.src= `${product.imageUrl}`;
-    img.alt= `${product.altTxt}`;
-    divImg.appendChild(img);
+function showProduct(product) {
 
-    let title = document.getElementById("title");
-    title.innerText = `${product.name}`;
+  let divImg = document.querySelector(".item__img");
+  let img = document.createElement("img");
+  img.src = `${product.imageUrl}`;
+  img.alt = `${product.altTxt}`;
+  divImg.appendChild(img);
 
-    let price = document.getElementById("price");
-    price.innerText = new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(product.price); 
+  let title = document.getElementById("title");
+  title.innerText = `${product.name}`;
 
-    let description = document.getElementById("description");
-    description.innerText = `${product.description}`;
+  let price = document.getElementById("price");
+  price.innerText = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(product.price);
 
-    for(let colors in product.colors){
-        let color = document.getElementById("colors");
-        color.innerHTML += `<option value="${product.colors[colors]}">${product.colors[colors]}</option>`;
-    }
+  let description = document.getElementById("description");
+  description.innerText = `${product.description}`;
+
+  for (let colors in product.colors) {
+    let color = document.getElementById("colors");
+    color.innerHTML += `<option value="${product.colors[colors]}">${product.colors[colors]}</option>`;
+  }
 }
+
+function itemQuantity() {
+  let quantity = document.getElementById("quantity");
+  return quantity.value;
+}
+
+function itemColor() {
+  let color = document.getElementById("colors");
+  return color.value;
+}
+
+
+const addToCartBtn = document.getElementById("addToCart");
+addToCartBtn.addEventListener("click", () => {
+  let quantity = parseInt(itemQuantity());
+  let color = itemColor();
+  addToCart(id, color, quantity);
+});
+
+function addToCart(id, color, quantity) {
+
+  while (color == "" || quantity <= 0 || quantity >= 101 || Number.isNaN(quantity)) {
+    if (color == "") {
+      alert("Veuillez sélectionner une couleur.");
+    }
+    else if (Number.isNaN !== quantity) {
+      alert("L'entrée saisie est incorrecte.");
+    }
+    else if (quantity <= 0 || quantity >= 101) {
+      alert("Veuillez choisir une quantité entre 1 et 100.");
+    }
+  
+    return;
+  }
+  console.log(`ajout de :${quantity} Kanap ${color} `);
+  alert("L'article a été ajouté au panier.")
+  
+} 
--- front/js/script.js
+++ front/js/script.js
@@ -5,35 +5,35 @@ fetch("http://localhost:3000/api/products/")
     .then((product) => product.json())
     .then((products) => {
 
-    showProducts(products)
-        
+        showProducts(products)
+
     })
 
-    //products insertion into "items" blocs
-    function showProducts(products){
-        
-        for(let product of products) {
-            let sectionItems= document.getElementById("items");
-            let a = document.createElement("a");
-            a.href=`product.html?id=${product._id}`
-            sectionItems.appendChild(a);
+//products insertion into "items" blocs
+function showProducts(products) {
 
-            let article = document.createElement("article");
-            a.appendChild(article);
+    for (let product of products) {
+        let sectionItems = document.getElementById("items");
+        let a = document.createElement("a");
+        a.href = `product.html?id=${product._id}`
+        sectionItems.appendChild(a);
 
-            let img = document.createElement("img");
-            img.src= `${product.imageUrl}`;
-            img.alt= `${product.altTxt}`;
-            article.appendChild(img);
+        let article = document.createElement("article");
+        a.appendChild(article);
 
-            let h3 = document.createElement("h3");
-            h3.classList.add("ProductName");
-            h3.innerText = `${product.name}`
-            article.appendChild(h3);
+        let img = document.createElement("img");
+        img.src = `${product.imageUrl}`;
+        img.alt = `${product.altTxt}`;
+        article.appendChild(img);
 
-            let p = document.createElement("p");
-            p.classList.add("ProductDescription");
-            p.innerText = `${product.description}`
-            article.appendChild(p);
-        };    
-    }
+        let h3 = document.createElement("h3");
+        h3.classList.add("ProductName");
+        h3.innerText = `${product.name}`
+        article.appendChild(h3);
+
+        let p = document.createElement("p");
+        p.classList.add("ProductDescription");
+        p.innerText = `${product.description}`
+        article.appendChild(p);
+    };
+}
