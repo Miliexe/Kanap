3a9d44d2f4351395ecb55aba5af904cf15f10369
Miliexe <emilie.lblc@gmail.com>
2022-08-23 17:12:42 +0200

    Update getCart + Init showCart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,63 @@
 console.log("Page pannier");
 
-getCart();
+showCart();
 
 function getCart() {
-    for (var i = 1; i <= localStorage.length; i++){
-        let cart = JSON.parse(localStorage.getItem(`item${i}`));
-        console.log(cart);
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (cart == null) {
+      cart = [];
+    }
+    console.log(cart);
+    return cart;
+  }
+
+function showCart(){
+    let cart = getCart();
+    for( var i = 0; i < cart.length; i++){
+        let item = cart[i];
+        console.log(item.id);
+        fetch(`http://localhost:3000/api/products/${item.id}`)
+            .then((data) => data.json())
+            .then((product) => {
+
+            showItem(product, item.quantity, item.color);
+
+        })
+        
     }
 }
+
+function showItem(item, quantity, color){
+    let sectionItems = document.getElementById("cart__items");
+    let article = document.createElement("article");
+    article.classList.add("cart__item__img");
+    article.dataset.id = `${item._id}`
+    article.dataset.color = `${color}`
+    sectionItems.appendChild(article);
+
+
+    
+}
+
+
+  /*  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+  <div class="cart__item__img">
+    <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+  </div>
+  <div class="cart__item__content">
+    <div class="cart__item__content__description">
+      <h2>Nom du produit</h2>
+      <p>Vert</p>
+      <p>42,00 €</p>
+    </div>
+    <div class="cart__item__content__settings">
+      <div class="cart__item__content__settings__quantity">
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+      </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+      </div>
+    </div>
+  </div>
+</article> -->*/
